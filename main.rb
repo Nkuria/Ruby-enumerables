300ab0ae12376d79165257c90a25cf1bd8c10445
Nkuria <nkuria01@gmail.com>
2020-10-08 09:28:14 +0300

    Recreated my_all, my_any, my_inject methods to correct reviewers issues

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+# rubocop: disable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
+# rubocop:disable Metrics/ModuleLength
 module Enumerable
   def my_each
     return to_enum unless block_given?
@@ -34,20 +36,44 @@ module Enumerable
   end
   [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].my_select { |x| puts x if x.even? }
 
-  def my_all?
+  def my_all(arg)
+    arr = to_a
     if block_given?
-      my_each { |x| return true if yield(x) == true }
+      arr.my_each { |x| return true unless yield(x) == false }
+    elsif arg.empty
+      return false
+    elsif !arg.empty? && (arg.is_a? Regexp)
+      arr.my_each { |x| return false unless x.match(arg) }
+    elsif (arg.is_a? Class) && !arg.empty?
+      arr.my_each { |x| return false unless [x.class, x.class.superclass].include?(arg) }
     else
-      my_each { |x| return true if x == true }
+      to_a.my_each { |i| return false if i != arg }
+
     end
     true
   end
 
-  def my_any?
+  def my_any?(arg)
     if block_given?
-      my_each { |x| return false unless yield(x) == true }
+      my_each do |x|
+        return true if yield x
+      end
+
+    elsif !arg.nil? && (arg.is_a? Class)
+      my_each do |x|
+        return false unless x.class == arg
+      end
+
+    elsif (arg.class == Regexp) && !arg.nil?
+      my_each do |x|
+        return false unless arg.match(x)
+      end
+
     else
-      my_each { |x| return false unless x == true }
+      my_each do |x|
+        return true if x == arg
+      end
+
     end
     false
   end
@@ -65,30 +91,51 @@ module Enumerable
     elsif num.nil?
       c = length
     else
-      my_each { |z| x += 1 if z == num }
+      my_each { |z| x += 1 if z == args }
     end
     c
   end
 
-  def my_map(args)
-    final = []
-    if args == true
-      my_each do |x|
-        final.push(args).call(x)
-      end
+  def my_map(proc = nil)
+    return to_enum(:my_map) unless block_given? || !proc.nil?
 
-      final
+    final = []
+    arr = to_a
+
+    if block_given? && proc.nil?
+      arr.my_each { |x| final.push(yield(x)) }
+    elsif block_given? && !proc.nil?
+      arr.my_each { |x| final.push(proc.call(x)) }
     else
-      self
+      arr.my_each { |x| final.push(proc.call(x)) }
+
+    end
+    final
+  end
+
+  def my_inject(num = nil, symb = nil)
+    if block_given?
+      acc = num
+      my_each { |x| acc = acc.nil? ? x : yield(acc, x) }
+      acc
+    elsif symb.is_a?(Symbol) || symb.is_a?(String)
+      acc = num
+      my_each { |x| acc = acc.nil? ? x : acc.send(symb, x) }
+      acc
+    elsif num.is_a?(String) || num.is_a?(Symbol)
+      acc = nil
+      my_each { |x| acc = acc.nil? ? x : acc.send(num, x) }
+      acc
+
+    else
+      raise LocalJumpError, 'No block Given or Empty Argument' unless !num.nil? && !symb.nil? && !block_given?
     end
   end
-
-  def my_inject(args = nil)
-    acc = args.nil? ? first : args
-    my_each { |x| acc = yield(acc, x) }
-    acc
-  end
 end
+
+# rubocop:enable Metrics/PerceivedComplexity
+# rubocop:enable Metrics/CyclomaticComplexity
+# rubocop:enable Metrics/ModuleLength
 
 def multiply_els
   my_inject(5, :*)
