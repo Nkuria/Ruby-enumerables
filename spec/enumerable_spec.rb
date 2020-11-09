eec90308d0e41508cdddef245ca0fd8348c79a52
smcommits <sm.anas@outlook.com>
2020-11-09 19:37:27 +0530

    Add test for my_count, my_any, and my_none
    Added the test case for my_count, my_any, and my_none with three test conditions
     Condition 1: No block given
     Condtiion 2: Block given
     Condtiion 3: Argument Passed

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,4 +1,4 @@
-require_relative '../main.rb'
+require_relative '../main'
 
 describe Enumerable do
   describe '#my_each' do
@@ -45,6 +45,48 @@ describe Enumerable do
 
     it 'If a pattern is supplied returns whether the pattern matches for every collection member' do
       expect([1, 2i, 3.14].my_all?(Numeric)).to eql(true)
+    end
+  end
+
+  describe '#my_any' do
+    it 'returns true if no block is given and atleast one of the collection member is not false' do
+      expect([nil, true, 99].my_any?).to eql(true)
+    end
+
+    it 'returns true if f the block ever returns a value other than false or nil' do
+      expect(%w[ant bear cat].my_any? { |word| word.length >= 4 }).to eql(true)
+    end
+
+    it 'returns true if the pattern matches any of the element in the collection' do
+      expect([nil, true, 99].my_any?(Integer)).to eql(true)
+    end
+  end
+
+  describe '#my_none' do
+    it 'If the block is not given, will return true only if none of the collection members is true.' do
+      expect([nil, false, true].my_none?).to eql(false)
+    end
+
+    it 'returns true if the block never returns true for all elements' do
+      expect(%w[ant bear cat].my_none? { |word| word.length == 5 }).to eql(true)
+    end
+
+    it 'returns true if the pattern matches none of the element in the collection' do
+      expect([1, 3.14, 42].my_none?(Float)).to eql(false)
+    end
+  end
+
+  describe '#my_count' do
+    it 'will return the length of the collection if no block is given' do
+      expect([1, 2, 4, 2].my_count).to eql(4)
+    end
+
+    it 'returns the number of items in enum through enumeration' do
+      expect([1, 2, 4, 2].my_count { |x| x < 4 }).to eql(3)
+    end
+
+    it 'If an argument is given, the number of items in enum that are equal to item are counted' do
+      expect([1, 2, 4, 2].my_count(2)).to eql(2)
     end
   end
 end
